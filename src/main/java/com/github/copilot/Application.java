f0cbddd98cea7ee142a8085b0e185cec954a7c39
zxbdmz2012 <3136803133@qq.com>
2024-07-13 14:58:37 +0800

    fix
    fix

--- src/main/java/com/github/copilot/Application.java
+++ src/main/java/com/github/copilot/Application.java
@@ -1,24 +1,24 @@
-package com.github.copilot;
-
-import com.github.copilot.configcenter.client.annotation.EnableConfigClient;
-import com.github.copilot.configcenter.server.annotation.EnableConfigServer;
-import com.github.copilot.exceptionhandler.annotation.EnableGlobalExceptionHandler;
-import com.github.copilot.user.annotation.EnableUserInfoArgResolver;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@EnableUserInfoArgResolver
-@EnableScheduling
-@EnableGlobalExceptionHandler
-//@EntityScan(basePackages = {"com.github.copilot.task.entity", "com.github.copilot.configcenter.entity","com.github.copilot.exceptionhandler.entity"})
-//@EnableJpaRepositories({"com.github.copilot.task.repository", "com.github.copilot.configcenter.repository","com.github.copilot.exceptionhandler.repository"})
-
-@EnableConfigServer
-@EnableConfigClient
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-}
+//package com.github.copilot;
+//
+//import com.github.copilot.configcenter.client.annotation.EnableConfigClient;
+//import com.github.copilot.configcenter.server.annotation.EnableConfigServer;
+//import com.github.copilot.exceptionhandler.annotation.EnableGlobalExceptionHandler;
+//import com.github.copilot.user.annotation.EnableUserInfoArgResolver;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//
+//@SpringBootApplication
+//@EnableUserInfoArgResolver
+//@EnableScheduling
+//@EnableGlobalExceptionHandler
+////@EntityScan(basePackages = {"com.github.copilot.task.entity", "com.github.copilot.configcenter.entity","com.github.copilot.exceptionhandler.entity"})
+////@EnableJpaRepositories({"com.github.copilot.task.repository", "com.github.copilot.configcenter.repository","com.github.copilot.exceptionhandler.repository"})
+//
+//@EnableConfigServer
+//@EnableConfigClient
+//public class Application {
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+//}
